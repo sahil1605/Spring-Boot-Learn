72a495f8163817748df0baa91ece87f686c86e16
SAHIL AGRAWAL <f20190069@pilani.bits-pilani.ac.in>
2025-05-05 23:13:12 +0530

    added

--- src/main/java/com/learntest/demo/repository/CategoryRepository.java
+++ src/main/java/com/learntest/demo/repository/CategoryRepository.java
@@ -1,10 +1,17 @@
 package com.learntest.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.learntest.demo.entity.Category;
-
+// implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
+//     implementation 'org.postgresql:postgresql'
 
 @Repository
-public interface CategoryRepository extends CrudRepository<Category, Long> {}
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByName(String categoryName);
+    void deleteByName(String categoryName);
+}
